7309200f1555a574c3b78cd98b3ad0edad57c532
Clinton Moshe <clintonmwachia9@gmail.com>
2026-03-13 16:18:17 +0300

    Load the Titanic data

--- scripts/titanic/titanic_data_cleaning.R
+++ scripts/titanic/titanic_data_cleaning.R
@@ -1,1 +1,13 @@
-print("titanic data")
+# Cleaning Titanic Data set
+
+# loading the libraries
+library(tidyverse)
+
+# load the raw data
+titanic <- read_csv("data/raw/titanic/titanic_data.csv", show_col_types = FALSE)
+
+# structure of data
+str(titanic)
+
+# summary of data
+summary(titanic)
